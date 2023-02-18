c174238ef551efd7b2948eaa35d8fd850b60f956
Nguyen Van Vy <nguyenvanvy1999@gmail.com>
2023-02-18 11:40:55 +0700

    fix: fix control

--- esp_control/main/main.ino
+++ esp_control/main/main.ino
@@ -5,13 +5,19 @@
 #include <addons/TokenHelper.h>
 #include <addons/RTDBHelper.h>
 #include <Servo.h>
-#define WIFI_SSID "Tenda_CB9AA0"
+
+#define WIFI_SSID "MERCUSYS_95AA"
 #define WIFI_PASSWORD "phamducan"
 
 #define API_KEY "AIzaSyA-5IRQGWEJLblqImYz_2s9Jw16-LXOA50"
 #define DATABASE_URL "doan-eed4a-default-rtdb.asia-southeast1.firebasedatabase.app"
 #define USER_EMAIL "esp8266@email.com"
 #define USER_PASSWORD "123456"
+
+#define TRIGGER1 D1
+#define ECHO1 D2
+#define TRIGGER2 D5
+#define ECHO2 D6
 
 FirebaseData fbdo;
 FirebaseAuth auth;
@@ -36,9 +42,38 @@ void closeOutDoor() {
   servoOut.write(-90);
 }
 
+int getDistanceSensor(int sensor) {
+  unsigned long duration;
+  int distance;
+  if (sensor == 1) {
+    digitalWrite(TRIGGER1, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIGGER1, HIGH);
+    delayMicroseconds(5);
+    digitalWrite(TRIGGER1, LOW);
+    duration = pulseIn(ECHO1, HIGH);
+  } else if (sensor == 2) {
+    digitalWrite(TRIGGER2, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIGGER2, HIGH);
+    delayMicroseconds(5);
+    digitalWrite(TRIGGER2, LOW);
+    duration = pulseIn(ECHO2, HIGH);
+  }
+
+  distance = int(duration / 2 / 29.412);
+  Serial.printf("Distance %d", distance);
+  return distance;
+}
+
+
 void setup() {
   servoIn.attach(15);   // NodeMCU D8 (GP15) pin
   servoOut.attach(13);  // NodeMCU D7 (GP13) pin
+  pinMode(TRIGGER1, OUTPUT);
+  pinMode(ECHO1, INPUT);
+  pinMode(TRIGGER2, OUTPUT);
+  pinMode(ECHO2, INPUT);
   Serial.begin(9600);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-Fi");
@@ -83,5 +118,12 @@ void loop() {
     } else if (controlOut == "O") {
       openOutDoor();
     }
+    int distance1 = getDistanceSensor(1);
+    int distance2 = getDistanceSensor(2);
+    Serial.printf("Distance 1...", distance1);
+    Serial.printf("Distance 2...", distance2);
+
+    Serial.printf("Set distance 1... %s\n", Firebase.RTDB.setInt(&fbdo, F("/out_distance_1"), distance1) ? "ok" : fbdo.errorReason().c_str());
+    Serial.printf("Set distance 2... %s\n", Firebase.RTDB.setInt(&fbdo, F("/out_distance_2"), distance2) ? "ok" : fbdo.errorReason().c_str());
   }
 }
